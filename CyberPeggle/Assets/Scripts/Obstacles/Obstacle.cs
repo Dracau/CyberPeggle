6d13bb23cad630d42744e0c3fe4ceb563443f47a
Gautier DEBREU <g.debreu@rubika-edu.com>
2022-11-28 15:45:17 +0100

    Delay before disappearing obstacles

--- CyberPeggle/Assets/Scripts/Obstacles/Obstacle.cs
+++ CyberPeggle/Assets/Scripts/Obstacles/Obstacle.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField] private float timeBeforeDisappearing;
+    private bool hitted;
     public virtual void Hit()
     {
+        if(hitted) return;
+        hitted = true;
         Debug.Log(gameObject.name + " was hit");
+        StartCoroutine(Disappear());
+    }
+
+    private IEnumerator Disappear()
+    {
+        GetComponent<SpriteRenderer>().color = Color.black;
+        yield return new WaitForSeconds(timeBeforeDisappearing);
         Destroy(gameObject);
     }
 }
